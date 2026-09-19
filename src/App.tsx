61c49bf4170f17572f452e35f268a02c5d438c13
Ameen-Bro <ameeeeeen2341@gmail.com>
2026-09-19 19:17:46 +0530

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { History } from './pages/History';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
